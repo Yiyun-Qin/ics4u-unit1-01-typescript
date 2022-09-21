3be84f0a3919552e0fdaecabc89a46cf08565798
Yiyun Qin <yiyun.qin@mths.ca>
2022-09-21 13:15:55 +0000

    type

--- src/index.ts
+++ src/index.ts
@@ -10,25 +10,27 @@
 import promptSync from 'prompt-sync'
 
 const prompt = promptSync()
-let log_number: number | undefined
-let car_weight = 1100
-let weight_metre = 20
+let logNumber: number | undefined
+const carWeight = 1100
+const weightMetre = 20
 
 console.log('There are three types of log length. 0.25m, 0.5m and 1m.')
 const type = prompt('Which type of length you want to choose?(Enter number): ')
 
-if (type == '0.25') {
-  log_number = car_weight / weight_metre / 0.25
-} else if (type == '0.5') {
-  log_number = car_weight / weight_metre / 0.5
-} else if (type == '1') {
-  log_number = car_weight / weight_metre / 1
+if (type === '0.25') {
+  logNumber = carWeight / weightMetre / 0.25
+} else if (type === '0.5') {
+  logNumber = carWeight / weightMetre / 0.5
+} else if (type === '1') {
+  logNumber = carWeight / weightMetre / 1
 } else {
   console.log('\nPlease choose a type in options!')
 }
 
-if (type == '0.25' || type == '0.5' || type == '1') {
-  console.log(`\n${log_number} logs need to be placed on the truck.`)
+if (type === '0.25' || type === '0.5' || type === '1') {
+  console.log(`\n${logNumber} logs need to be placed on the truck.`)
+} else if (logNumber === undefined) {
+  console.log('Please enter a number!')
 }
 
 console.log('\nDone.')
