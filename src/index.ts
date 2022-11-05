9f468f1228334ce453dfed824ccac4fc8097f506
Yiyun Qin <yiyun.qin@mths.ca>
2022-11-05 21:07:27 +0000

    log number

--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,19 @@ const prompt = promptSync()
 let logNumber: number | undefined
 const carWeight = 1100
 const weightMetre = 20
+const option1 = 0.25
+const option2 = 0.5
+const option3 = 1
 
 console.log('There are three types of log length. 0.25m, 0.5m and 1m.')
 const type = prompt('Which type of length you want to choose?(Enter number): ')
 
 if (type === '0.25') {
-  logNumber = carWeight / weightMetre / 0.25
+  logNumber = carWeight / weightMetre / option1
 } else if (type === '0.5') {
-  logNumber = carWeight / weightMetre / 0.5
+  logNumber = carWeight / weightMetre / option2
 } else if (type === '1') {
-  logNumber = carWeight / weightMetre / 1
+  logNumber = carWeight / weightMetre / option3
 } else {
   console.log('\nPlease choose a type in options!')
 }
